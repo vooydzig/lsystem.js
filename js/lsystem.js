a0dfc6384f952ef13e4f60f5a3cc181e13068101
wojcikt <wojcikt@tt.com.pl>
2016-03-14 15:07:37 +0100

    added prev/next simulation steps

--- js/lsystem.js
+++ js/lsystem.js
@@ -1,6 +1,5 @@
 function LSystem(start, options) {
-    this.start = start;
-    this.system = this.start;
+    this.systems = [start];
     this.step = 0;
     this.rules = {};
     this.variables = {};
@@ -47,6 +46,10 @@ function LSystem(start, options) {
         that.turtle.position = state.position;
         that.turtle.angle= state.angle;
     }
+    
+    function getCurrentStep(that) {
+        return that.systems[that.step];
+    }
 
     LSystem.prototype.addRule = function(name, value) {
         this.rules[name] = value;
@@ -54,25 +57,38 @@ function LSystem(start, options) {
 
     LSystem.prototype.draw = function() {
         this.turtle.reset();
-        for( var i =0; i<this.system.length; i++) {
-            var c = this.system[i];
+        var current = getCurrentStep(this);
+        for( var i =0; i<current.length; i++) {
+            var c = current[i];
             if (this.variables.hasOwnProperty(c))
                 this.variables[c](this);
             else if (this.constants.hasOwnProperty(c))
                 this.constants[c](this);
         }
     };
-
-    LSystem.prototype.simulate = function() {
+    
+    LSystem.prototype.next = function() {
+        var system = this.systems[this.step+1];
+        if (system) {
+            this.step += 1;
+            return;
+        }
+        
+        var current = getCurrentStep(this);
         var next = '';
-        for( var i =0; i<this.system.length; i++) {
-            var c = this.system[i];
+        for( var i =0; i<current.length; i++) {
+            var c = current[i];
             if (this.rules.hasOwnProperty(c))
                 next += this.rules[c];
             else if (this.constants.hasOwnProperty(c))
                 next += c;
         }
-        this.system = next;
+        this.systems.push(next);
         this.step += 1;
-    };
+    }
+    
+    LSystem.prototype.prev = function() {
+        this.step = Math.max(0, this.step-1);
+    }
+
 })();
